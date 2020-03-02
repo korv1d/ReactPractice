04f388d0870983cdfea03debf52b6f3e5c5e0034
Ayden <aydenchubbic@gmail.com>
2020-03-01 23:41:43 -0800

    init project, trying react on SPM platform

---
+++ toDoList/index.js
@@ -0,0 +1,8 @@
+//note: include React and React-DOM in dependencies of project
+import React from "react"
+import ReactDOM from "react-dom"
+
+//Takes two arguments
+// 1) What you want to render
+// 2) Where you want to render it
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"))
