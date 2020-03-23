8af76757c15944a51075a1d6a5c2a09d780ae18d
Ayden <aydenchubbic@gmail.com>
2020-03-22 21:45:44 -0700

    STANDO POWAH

--- playground/src/App.js
+++ playground/src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://imgur.com/gallery/klVL7XQ"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          But what if...
         </a>
       </header>
     </div>
