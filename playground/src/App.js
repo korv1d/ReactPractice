7913648afb2d8a0a62af84c01292b947fdf69546
Ayden <aydenchubbic@gmail.com>
2020-03-22 21:50:55 -0700

    unordered list

--- playground/src/App.js
+++ playground/src/App.js
@@ -10,6 +10,11 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          <li> Kono </li>
+          <li> Dio </li>
+          <li> Da! </li>
+        </ul>
         <a
           className="App-link"
           href="https://imgur.com/gallery/klVL7XQ"
