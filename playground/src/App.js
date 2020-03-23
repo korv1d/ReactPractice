3b513076b41f9354da095fad48d0ce8e2925e198
Ayden <aydenchubbic@gmail.com>
2020-03-22 22:40:45 -0700

    clean revert

--- playground/src/App.js
+++ playground/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={inigo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Inigo Montoya's guide to networking success:
         </p>
