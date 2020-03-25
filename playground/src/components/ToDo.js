86ddf464a96d4110db5cbf0e9bea583e96327ea6
Ayden <aydenchubbic@gmail.com>
2020-03-25 00:44:59 -0700

    primitive ToDo list

---
+++ playground/src/components/ToDo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function ToDo() {
+	return (
+	
+		<div>
+			<input type = "checkbox" />
+			<p> Item 1 </p>
+			
+			<input type = "checkbox" />
+			<p> Item 2 </p>
+			
+			<input type = "checkbox" />
+			<p> Item 2 </p>
+			
+			<input type = "checkbox" />
+			<p> Item 2 </p>
+			
+		</div>
+	
+	);
+}
+
+export default ToDo;
