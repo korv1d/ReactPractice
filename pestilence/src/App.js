63236ff4638c1df0ddebba3ca9dc41b554ddef9a
Ayden <aydenchubbic@gmail.com>
2020-03-27 20:21:23 -0700

    changed App to class components layout, added Toolbar.css

--- pestilence/src/App.js
+++ pestilence/src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+//each App file should contain only one React components
+
+import React, {Component} from 'react';
+import Toolbar from './components/Toolbar.js'
 import './App.css';
 
-import OurMission from './components/OurMission.js';
-import WhyLA from './components/WhyLA.js';
-import Help from './components/Help.js';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <OurMission />
-        <WhyLA />
-        <Help />
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className = "App">
+				<header className="App-header">
+					<Toolbar />
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
