b0cfa3a731a78c1904223186586758309d0bcf53
Ayden <aydenchubbic@gmail.com>
2020-04-08 23:59:33 -0700

    removed router function

--- pestilence/src/App.js
+++ pestilence/src/App.js
@@ -1,18 +1,30 @@
-//each App file should contain only one React components
+// each App file should contain only one React components
 
 import React, {Component} from 'react';
-import Toolbar from './components/Toolbar.js';
-import SideDrawer from './components/SideDrawer.js';
+import logo from './logo.svg';
+
 import './App.css'
 
 class App extends Component {
+
+  var express = require('express');
+  var router = express.Router();
+  var exec = require('child_process') exec;
+
+  //rounter.get('/', function(req, res, next) {
+    exec('ls', function(err, stdout, stderr){
+      console.log(stdout); 
+    })})
 	render() {
 		return (
 			<div className = "App" style={{height: '100%'}}>
-				<Toolbar />
-				<SideDrawer />
 				<header className="App-header">
-					
+          
+					<p> back to basics </p>
+					<button type="button">
+						Click me!
+            {console.log("Hello!")}
+					</button>
 				</header>
 			</div>
 		);
