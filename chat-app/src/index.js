e4c2b3fc7190d525cde429b9fa833a11fd13de8a
Ayden <aydenchubbic@gmail.com>
2020-04-10 23:19:45 -0700

    added firebase initialization code and API key

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCHK3FZLIhK7Pld4sKujAJFH1oOstcXC1w",
+  authDomain: "im-tutorial-bbb9b.firebaseapp.com",
+  databaseURL: "https://im-tutorial-bbb9b.firebaseio.com",
+  projectId: "im-tutorial-bbb9b",
+  storageBucket: "im-tutorial-bbb9b.appspot.com",
+  messagingSenderId: "983991027486",
+  appId: "1:983991027486:web:6a40d6cf05b7d71a532b0f",
+  measurementId: "G-17F5XG7NLK"
+})
+
 ReactDOM.render(<div>Hello World</div>,
   // <React.StrictMode>
   //   <App />
