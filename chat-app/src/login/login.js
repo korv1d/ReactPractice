bb619320000b5e191d8786183c04b31957a97ae4
Ayden <aydenchubbic@gmail.com>
2020-04-12 23:41:28 -0700

    boilerplate for login

---
+++ chat-app/src/login/login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class LoginComponent extends React.component {
+
+    render() {
+        return(<div>Hello world from Login</div>);
+    }
+}
+
+export default LoginComponent
