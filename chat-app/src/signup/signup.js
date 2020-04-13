a7c0a6e569430d67ddcd4be45c52e76aa81920ac
Ayden <aydenchubbic@gmail.com>
2020-04-12 23:42:09 -0700

    boilerplate for signup

---
+++ chat-app/src/signup/signup.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class SigninComponent extends React.component {
+
+    render() {
+        return(<div>Hello world from Signin</div>);
+    }
+}
+
+export default SigninComponent
