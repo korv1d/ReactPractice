0080c7665add4c8bcfabc0621e5957602b1635eb
Ayden <aydenchubbic@gmail.com>
2020-04-13 00:29:23 -0700

    added materialUI packages

--- chat-app/src/signup/signup.js
+++ chat-app/src/signup/signup.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/withStyle';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-class SigninComponent extends React.component {
+class SignupComponent extends React.Component {
 
     render() {
         return(<div>Hello world from Signin</div>);
     }
 }
 
-export default SigninComponent
+export default SignupComponent;
