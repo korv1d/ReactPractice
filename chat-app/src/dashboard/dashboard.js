7e791c3eb78ffd26e83f72e14e70c614379954e0
Ayden <aydenchubbic@gmail.com>
2020-04-12 23:44:46 -0700

    added router and boilerplate dash

---
+++ chat-app/src/dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class DashboardComponent extends React.component {
+
+    render() {
+        return(<div>Hello world from Dashboard</div>);
+    }
+}
+
+export default DashboardComponent
