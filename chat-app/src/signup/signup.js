348bd58e913bd166ca44d27fb8a206ab1e9a3ebe
Ayden <aydenchubbic@gmail.com>
2020-04-14 22:15:39 -0700

    working signup boilerplate

--- chat-app/src/signup/signup.js
+++ chat-app/src/signup/signup.js
@@ -1,12 +1,16 @@
 import React from 'react';
+
+import styles from './styles';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Paper from '@material-ui/core/Paper';
-import withStyles from '@material-ui/core/withStyle';
+import withStyles from '@material-ui/core/styles/withStyles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+
+const firebase = require('firebase');
 
 class SignupComponent extends React.Component {
 
