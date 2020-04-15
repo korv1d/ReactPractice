57d03001580ca76f881e56847fb4470b29422719
Ayden <aydenchubbic@gmail.com>
2020-04-14 22:40:33 -0700

    form created, onChange.log works

--- chat-app/src/signup/signup.js
+++ chat-app/src/signup/signup.js
@@ -8,6 +8,7 @@ import Paper from '@material-ui/core/Paper';
 import withStyles from '@material-ui/core/styles/withStyles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
+// import { InputLabel } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 
 const firebase = require('firebase');
@@ -15,8 +16,39 @@ const firebase = require('firebase');
 class SignupComponent extends React.Component {
 
     render() {
-        return(<div>Hello world from Signin</div>);
+        //destructuring. can use classes. instead of this.props.classes
+        const { classes } = this.props;
+
+        return(
+            <main className = {classes.main}>
+                <CssBaseline>
+                    
+                </CssBaseline>
+                <Paper className={classes.Paper}>
+                    <Typography components='h1' variant='h5'>
+                        Sign up!
+                    </Typography>
+                    {/* by defualt submit event is passed as arg */}
+                    <form onSubmit={(e) => this.submitSignup(e)} className={classes.form}>
+                        <FormControl required fullWidth margin = 'normal'>
+                            <InputLabel htmlFor='signup-email-input'>Enter your Email</InputLabel>
+                            {/* autofocus = automatically awaiting text when user opens form */}
+                            {/* onChange = whenever user is typing, call this function */}
+                            <Input autoComplete='email' onChange={(e) => this.userTyping('email', e)} autofocus id='signup-email-input'></Input>
+                        </FormControl>
+                    </form>
+                </Paper>
+            </main>
+        );
+    }
+
+    userTyping = (type, e) => {
+        console.log(type, e);
+    }
+
+    submitSignup = (e) => {
+        console.log("submitting");
     }
 }
 
-export default SignupComponent;
+export default withStyles(styles)(SignupComponent);
