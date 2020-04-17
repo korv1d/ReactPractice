332c9125572d54cd70e43478648370e1aad45327
Ayden <aydenchubbic@gmail.com>
2020-04-16 23:27:33 -0700

    added login dependencies and basic frame

--- chat-app/src/login/login.js
+++ chat-app/src/login/login.js
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+import styles from './styles';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Paper from '@material-ui/core/Paper';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+const firebase = require("firebase");
+
 class LoginComponent extends React.Component {
 
     render() {
-        return(<div>Hello world from Login</div>);
+
+        const { classes } = this.props;
+        return(
+            <main className={classes.main}>
+                <CssBaseline></CssBaseline>
+                <Paper className={ classes.paper}>
+                    
+                </Paper>
+            </main>
+        );
     }
 }
 
-export default LoginComponent;
+export default withStyles(styles)(LoginComponent);
