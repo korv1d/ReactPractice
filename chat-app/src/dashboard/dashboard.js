087f91ad58de06b0f6dd8ae3a94535e300bf420a
Ayden <aydenchubbic@gmail.com>
2020-04-21 23:24:41 -0700

    boiler plate styling

--- chat-app/src/dashboard/dashboard.js
+++ chat-app/src/dashboard/dashboard.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ChatListComponent from '../chatList/chatList.js';
 
-class DashboardComponent extends React.component {
+class DashboardComponent extends React.Component {
 
     render() {
         return(<div>Hello world from Dashboard</div>);
     }
 }
 
-export default DashboardComponent
+export default DashboardComponent;
