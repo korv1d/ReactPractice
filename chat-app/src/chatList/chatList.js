3db0f5bf4c6834e1a15687bb147f2d006c8f3b97
Ayden <aydenchubbic@gmail.com>
2020-04-23 23:26:19 -0700

    display first 30 chars of messages for each _user item in array

--- chat-app/src/chatList/chatList.js
+++ chat-app/src/chatList/chatList.js
@@ -10,13 +10,57 @@ import styles from './styles';
 import Divider from '@material-ui/core/Divider';
 import Button from '@material-ui/core/Button';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-import NotificationImportant from '@material-ui/icons/NotificationImportant';
+// import NotificationImportant from '@material-ui/icons/NotificationImportant';
 
 class ChatListComponent extends React.Component {
 
     render() {
 
-        return();
+        const { classes } = this.props;
+
+        return(
+            <main className={classes.root}>
+                <Button variant='contained' fullWidth color='primary' className={classes.newChatBtn} onClick={this.newChat}></Button>
+                <List>
+                    {
+                        //return list items depending on what chat we're looking at
+                        this.props.chats.map((_chat, _index) => {
+                            return(
+                                <ListItem onClick={() => this.selectChat(_index)} className={classes.listIten} selected={this.props.selectedChatIndex === _index} alignItems='flex-start'>
+                                    {/* set profile pic to first letter of name */}
+                                    <ListItemAvatar>
+                                        <Avatar alt='Remy Sharp'>  
+                                            {/* 0th index = person talk to, split('') means break into array of one char elements */}
+                                            {_chat.users.filter(_user => _user !== this.props.userEmail)[0].split('')[0]}
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText primary={_chat.users.filter(_user => _user !== this.props.userEmail)[0]} secondary={
+                                        <React.Fragment>
+                                            <Typography component='span' color='textPrimary'>
+                                                {
+                                                    //display first 30 chars of last chat message
+                                                    _chat.messages[_chat.messages.length - 1].message.substring(0,30)
+                                                }
+                                            </Typography>
+                                        </React.Fragment>
+                                    }>
+                                    </ListItemText>
+                                </ListItem>
+                                <Divider></Divider>
+                            )
+                        })
+                    }
+                </List>
+            </main>
+        );
+    }
+
+    newChat = () => {
+        console.log('new chat click');
+    }
+
+    selectChat = (index) => {
+        console.log('select chat', index);
     }
 }
 
