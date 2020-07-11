7753d4efb44c51d53ffbfc27dd9b857011782f73
Ayden <aydenchubbic@gmail.com>
2020-07-11 14:09:03 -0700

    added README

--- expoTemplate/App.js
+++ expoTemplate/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  let x = 1;
   console.log("App executed");
 
   return (
---
+++ expoTemplate/README.md
@@ -0,0 +1,1 @@
+A simple mobile app that handles POST and GET commands from a reqres API. Compatible with both android and iOS.
